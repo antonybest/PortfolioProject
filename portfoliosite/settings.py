60cbd225e91bd8a85d35721a73d944a3114c3873
Antony Best <74147101+antonybest@users.noreply.github.com>
2021-01-07 19:20:06 +0000

    Update settings.py
    deleted secret key

--- portfoliosite/settings.py
+++ portfoliosite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm@z+@4zqzahd2&@kc+zy-q&&9)1fc%4h+i94(uq-_4++wa@8g1'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
